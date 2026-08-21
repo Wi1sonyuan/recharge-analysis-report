704f04a832c54a60b9e24370e6345c2243759d95
Wi1sonyuan <yuanday@vip.qq.com>
2026-08-21 15:46:32 +0800

    chore: 删除临时脚本

--- fix_kpi.py
+++
@@ -1,6 +0,0 @@
-with open('/tmp/fix3/magic_analysis_0821.html', 'r') as f:
-    c = f.read()
-c = c.replace('6.6% vs 上半年</div></div>\n\n<!-- PART 1', '6.6% vs 上半年</div></div>\n</div>\n\n<!-- PART 1')
-with open('/tmp/fix3/magic_analysis_0821.html', 'w') as f:
-    f.write(c)
-print('Fixed')
